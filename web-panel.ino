d48d47e08872165fc240344f1c0b661539a3f030
Niwantha <niwantha33@gmail.com>
2016-09-15 19:18:54 +0530

    Updated
    Need to change Go Back function to automatically return

--- web-panel.ino
+++ web-panel.ino
@@ -11,22 +11,27 @@ ESP8266WebServer server(80);
 const int led = 16;
 const int fan = 4;
 
-String con = "LED"; // to check condition 
+String con ;
+String con1 = "NOT SET";
+String con2 = "NOT SET";
+String con3 = "NOT SET";
+String con4 = "NOT SET";
+String con5 = "NOT SET";
 
 void handleRoot() {
 
   String page = "<h1> NodeMcu-Power Panel</h1>";
-  page += "<p>(PIN:DO) <a href=\"?pin=ON1\"><button>LAMP-ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>LAMP-OFF</button></a></p>";
-  page += "<p>(PIN:D2) <a href=\"?pin=ON2\"><button>FAN-ON</button></a>&nbsp;<a href=\"?pin=OFF2\"><button>FAN-OFF</button></a></p>";
-  page += "<p>(PIN:D1) <a href=\"?pin=ON3\"><button>LAMP-ON-2</button></a>&nbsp;<a href=\"?pin=OFF3\"><button>LAMP-OFF-2</button></a></p>";
-  page += "<p>(PIN:D4) <a href=\"?pin=ON4\"><button>LAMP-ON-3</button></a>&nbsp;<a href=\"?pin=OFF4\"><button>LAMP-OFF-3</button></a></p>";
-  page += "<p>(PIN:D5) <a href=\"?pin=ON5\"><button>LAMP-ON-4</button></a>&nbsp;<a href=\"?pin=OFF5\"><button>LAMP-OFF-4</button></a></p>";
-  page += "<p>(PIN:D6) <a href=\"?pin=ON6\"><button>LAMP-ON-5</button></a>&nbsp;<a href=\"?pin=OFF6\"><button>LAMP-OFF-5</button></a></p>";
-  page += "<p><center>Status <a href=\"?details\"><button>DETAILS</button></a><center>";
-  page += "<h2> Algobel Team : Free to Use </h2>";
+  page += "<p>(PIN:DO) <a href=\"pin=ON1\"><button>LAMP-ON</button></a>&nbsp;<a href=\"pin=OFF1\"><button>LAMP-OFF</button></a></p>";
+  page += "<p>(PIN:D2) <a href=\"pin=ON2\"><button>FAN-ON</button></a>&nbsp;<a href=\"pin=OFF2\"><button>FAN-OFF</button></a></p>";
+  page += "<p>(PIN:D1) <a href=\"pin=ON3\"><button>LAMP-ON-2</button></a>&nbsp;<a href=\"pin=OFF3\"><button>LAMP-OFF-2</button></a></p>";
+  page += "<p>(PIN:D4) <a href=\"pin=ON4\"><button>LAMP-ON-3</button></a>&nbsp;<a href=\"pin=OFF4\"><button>LAMP-OFF-3</button></a></p>";
+  page += "<p>(PIN:D5) <a href=\"pin=ON5\"><button>LAMP-ON-4</button></a>&nbsp;<a href=\"pin=OFF5\"><button>LAMP-OFF-4</button></a></p>";
+  page += "<p>(PIN:D6) <a href=\"pin=ON6\"><button>LAMP-ON-5</button></a>&nbsp;<a href=\"pin=OFF6\"><button>LAMP-OFF-5</button></a></p>";
+  page += "<p><center>Status <a href=\"details\"><button>DETAILS</button></a><center>";
+  page += "<h2> Algobel Team : Original Board Now You can Buy From Us </h2>";
   page += "<h3> Warning !!!!! </h3>";
-  page +="<font color=\"red\">Before Connecting to the :Relay board: please follow the proper instructions!</font>";
- // page += "<p> Before Connect to Relay board please follow the proper instructions </p>";
+  page += "<font color=\"red\">Before Connecting to the :Relay board: please follow the proper instructions!</font>";
+  // page += "<p> Before Connect to Relay board please follow the proper instructions </p>";
   server.send(200, "text/html", page);
 }
 
@@ -66,51 +71,78 @@ void setup(void) {
   server.on("/pin=ON1", []() {
     digitalWrite(led, 1);
     Serial.println("LED-1 ON");
-    con = "LED-1-ON";
+    con1 = "LED-1-ON";
+    server.send(200, "text/html", "GO Back");
   });
   server.on("/pin=OFF1", []() {
     digitalWrite(led, 0);
     Serial.println("LED-1 OFF");
-    con = "LED-1-OFF";
+    con1 = "LED-1-OFF";
+    server.send(200, "text/html", "GO Back");
   });
   server.on("/pin=ON2", []() {
     digitalWrite(fan, 1);
     Serial.println("LED-2 ON");
-    con = "LED-2-ON";
+    con2 = "LED-2-ON";
+    server.send(200, "text/html", "GO Back");
   });
   server.on("/pin=OFF2", []() {
     digitalWrite(fan, 0);
     Serial.println("LED-2 OFF");
-    con = "LED-2-OFF";
+    con2 = "LED-2-OFF";
+    server.send(200, "text/html", "GO Back");
   });
-  server.on("/pin=ON3", []() {    
+  server.on("/pin=ON3", []() {
     Serial.println("LED-3 ON");
-    con = "LED-3-ON";
-    });
-  server.on("/pin=OFF3", []() {   
+    con3 = "LED-3-ON";
+    server.send(200, "text/html", "GO Back");
+  });
+  server.on("/pin=OFF3", []() {
     Serial.println("LED-3 OFF");
-    con = "LED-3-OFF";
+    con3 = "LED-3-OFF";
+    server.send(200, "text/html", "GO Back");
   });
-server.on("/pin=ON4", []() {    
+  server.on("/pin=ON4", []() {
     Serial.println("LED-4 ON");
-    con = "LED-4-ON";
-   }); 
-  server.on("/pin=OFF4", []() {   
+    con4 = "LED-4-ON";
+    server.send(200, "text/html", "GO Back");
+  });
+  server.on("/pin=OFF4", []() {
     Serial.println("LED-4 OFF");
-    con = "LED-4-OFF";
+    con4 = "LED-4-OFF";
+    server.send(200, "text/html", "GO Back");
   });
-  server.on("/pin=ON5", []() {    
+  server.on("/pin=ON5", []() {
     Serial.println("LED-5 ON");
-    con = "LED-5-ON";
-    });
-    
-  server.on("/pin=OFF5", []() {   
-    Serial.println("LED-5 OFF");
-    con = "LED-5-OFF";
+    con5 = "LED-5-ON";
+    server.send(200, "text/html", "GO Back");
   });
- server.on("/details", []() {   
+
+  server.on("/pin=OFF5", []() {
+    Serial.println("LED-5 OFF");
+    con5 = "LED-5-OFF";
+    server.send(200, "text/html", "GO Back");
+  });
+  server.on("/details", []() {
     Serial.println("details");
-    server.send(200, "text/plain", con);
+    con = "<h> Status of the Light & Fan Conditions </h>";
+    con += "<p> LAMP :";
+    con += con1;
+    con += "</p>";
+    con += "<p> LAMP :";
+    con += con2;
+    con += "</p>";
+    con += "<p> LAMP :";
+    con += con3;
+    con += "</p>";
+    con += "<p> LAMP :";
+    con += con4;
+    con += "</p>";
+    con += "<p> LAMP :";
+    con += con5;
+    con += "</p>";
+    con +="<h2> NodeMcu, ESP Wroom Modules You can Buy From US </h2>";
+    server.send(200, "text/html", con);
   });
 
   server.onNotFound(handleNotFound);
@@ -126,4 +158,3 @@ void loop(void) {
   server.handleClient();
 
 }
-
